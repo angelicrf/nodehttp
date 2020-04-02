a23d4edd997c5b03febf1adcbf12107af5663cdd
Your Name <angelicrf@gmail.com>
2020-04-02 12:57:21 -0700

    WMongo-colors

--- server.js
+++ server.js
@@ -4,16 +4,22 @@ const dotEnv = require('dotenv');
 const morgan = require('morgan');
 const logger = require('./middleware/logger');
 const bootcamps = require('./routes/bootcamp');
-
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 dotEnv.config({path: './config/config.env'});
-
+connectDB();
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'));
 }
 
 app.use('/api/v1/bootcamp', bootcamps);
 
-
 const port = process.env.PORT || 30025;
-app.listen(port, () => console.log(`the server is running on port ${process.env.NODE_ENV} ${port}`));
+const server = app.listen(port, () =>
+    console.log(`the server is running on port ${process.env.NODE_ENV} ${port}`.yellow.bold));
+
+process.on('UnhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red);
+    server.close(() => process.exit(1));
+});
