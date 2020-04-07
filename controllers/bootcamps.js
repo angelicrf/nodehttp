91c4daef84f95c047bd366d9bf75bb185b1d2d24
Your Name <angelicrf@gmail.com>
2020-04-07 12:06:35 -0700

    WSort-limit-pagination

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -5,15 +5,53 @@ const geocoder = require('../utils/geocoder');
 
 exports.getBootcamps = asyncHandler(async (req, res, next ) => {
         let query;
-        let queryStr = JSON.stringify(req.query);
+        const reqQuery = {...req.query};
+
+        const removeFields = ['select','sort','page','limit'];
+        removeFields.forEach(param => delete reqQuery[param]);
+
+        let queryStr = JSON.stringify(reqQuery);
 
         queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g,
             match => `$${match}`);
         query = Bootcamp.find(JSON.parse(queryStr));
 
-        const bootcamps = await query;
+        if(req.query.select){
+            const fields = req.query.select.split(',').join(' ');
+            console.log(fields);
+            query = query.select(fields);
+        }
+        if(req.query.sort){
+            const sortBy = req.query.sort.split(',').join(' ');
+            console.log(sortBy);
+            query = query.sort(sortBy);
+        }
+        else {
+            query = query.sort('-createdAt');
+        }
+        const page = parseInt(req.query.page, 10) || 1;
+        const limit = parseInt(req.query.limit, 10) || 30;
+        const startIndex = (page - 1) * limit;
+        const endIndex = page *limit;
+        const total = await Bootcamp.countDocuments();
+        query = query.skip(startIndex).limit(limit);
 
+        const bootcamps = await query;
+        const pagination = {};
+        if(endIndex < total){
+            pagination.next = {
+                page: page + 1,
+                limit
+            }
+        }
+        if(startIndex > 0){
+            pagination.prev ={
+                page: page - 1,
+                limit
+            }
+        }
         res.status(200).json({success: true,
+            pagination,
             data: bootcamps,
             msg: 'show all bootcamps',
             count: bootcamps.length});
